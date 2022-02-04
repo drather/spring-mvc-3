4c5e73b7bd3cba9a246d89e84b366a9d29a523b7
kks <drather@naver.com>
2022-02-04 13:47:45 +0900

    class7-6: 상품 상세 컨트롤러, 화면 구현

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -19,6 +20,8 @@ import java.util.List;
 public class BasicItemController {
 
     private final ItemRepository itemRepository;
+    private long itemId;
+    private Model model;
 
     @GetMapping
     public String items(Model model) {
@@ -27,6 +30,18 @@ public class BasicItemController {
 
         return "basic/items";
     }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+//    @GetMapping("/add")
+//    public String addForm() {
+//        return "basic/addForm";
+//    }
 
     /**
      * 테스트용 데이터
